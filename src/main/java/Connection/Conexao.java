342d2b682161e14a06a31f6d8d2c1a3117b9778a
jhon souza <jhonsouza3021@gmail.com>
2026-09-11 00:52:20 -0300

    Remove a senha código

--- src/main/java/Connection/Conexao.java
+++ src/main/java/Connection/Conexao.java
@@ -8,9 +8,6 @@ import java.sql.Statement;
 public class Conexao {
 
 
-    private static final String url = "jdbc:mysql://localhost:3306/sistemadealuguel";
-    private static final String user = "root";
-    private static final String password = "1234567";
 
 
     public static Connection getConnection() {
@@ -19,6 +16,9 @@ public class Conexao {
         } catch (SQLException e) {
             throw new RuntimeException("Erro ao conectar com o banco.", e);
         }
+        private static final String url = "...";
+        private static final String user = "root";
+        private static final String password = System.getnev("MYSQL_PASSWORD");
 
 
 
